d002580f85b9152b0ad2c38bc4ec7446ebf0fcc7
Chau Hoang <chau.ducgiang@gmail.com>
2021-07-04 16:32:03 +0700

    services

---
+++ app/Services/BaseService.php
@@ -0,0 +1,51 @@
+<?php 
+
+namespace App\Services;
+use Illuminate\Support\Facades\App;
+
+use Illuminate\Support\Facades\Storage;
+abstract class BaseService implements ServiceInterface {
+    
+    private $disk;
+    protected $path;
+
+    public function __construct()
+    {
+        if (App::environment(['local'])){
+            $this->disk = 'local';
+        }else{
+            $this->disk = 'S3';
+        }
+    }
+
+    /* Get path */
+    public function getPath(){
+        return $this->path;
+    }
+
+    public function upload()
+    {
+        $filePath = '';
+        if($file){
+            $filePath = Storage::disk($this->disk)->putFile($this->path, $file);
+        }
+        return $filePath;
+    }
+
+    public function download($file, $name = 'download')
+    {
+        $headers = ['Content-Type: application/php'];
+        if($file && Storage::disk($this->disk)->exists($file)){
+            return Storage::download($file, $name, $headers);
+        }
+        return false;
+    }
+
+    public function remove($file)
+    {
+        if($file && Storage::disk($this->disk)->exists($file)){
+            return Storage::disk($this->disk)->delete($file);
+        }
+        return $false;
+    }
+}
---
+++ app/Services/ServiceInteface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Services;
+
+interface ServiceInterface{
+    public function upload($path);
+    public function download($path);
+    public function remove($path);
+}
